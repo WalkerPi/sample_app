204f2675f80688abc4632a0e1c48f67dd861cf0f
Rmandal <Rpgraf@gmail.com>
2016-05-03 14:32:18 -0500

    11.4 ready to go

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,6 +66,12 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
+  # Defines a proto-feed.
+  # See "following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
   #convert email to all lower-case
